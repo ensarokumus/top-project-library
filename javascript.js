9f2f201982e745c584718aa9c22b9462fe90634e
ensaro <41113027+ensarokumus@users.noreply.github.com>
2023-10-21 11:37:28 +1100

    Change Book object constructor to ES6 class

--- javascript.js
+++ javascript.js
@@ -5,15 +5,17 @@ const addBookModal = document.querySelector("#addBookModal");
 const addBookForm = document.querySelector("#addBookForm");
 const overlay = document.querySelector("#overlay");
 
-function Book(title, author, pages, read) {
-  this.title = title;
-  this.author = author;
-  this.pages = pages;
-  this.read = read;
-}
+class Book {
+  constructor(title, author, pages, read) {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = read;
+  }
 
-Book.prototype.toggleRead = function() {
-  this.read = !this.read;
+  toggleRead() {
+    return this.read = !this.read;
+  }
 }
 
 function addBookToLibrary() {
@@ -24,9 +26,9 @@ function addBookToLibrary() {
   let newBook = new Book(title, author, pages, read);
   myLibrary.push(newBook);
   render();
-  addBookModal.classList.remove('active');
-  overlay.classList.remove('active');
-};
+  addBookModal.classList.remove("active");
+  overlay.classList.remove("active");
+}
 
 addBookForm.addEventListener("submit", function (event) {
   event.preventDefault();
@@ -34,9 +36,9 @@ addBookForm.addEventListener("submit", function (event) {
 });
 
 addBookBtn.addEventListener("click", function () {
-  addBookForm.reset()
-  addBookModal.classList.add('active');
-  overlay.classList.add('active');
+  addBookForm.reset();
+  addBookModal.classList.add("active");
+  overlay.classList.add("active");
 });
 
 function removeBook(index) {
@@ -50,59 +52,58 @@ function toggleRead(index) {
 }
 
 function render() {
-  const booksGrid = document.querySelector('.books-grid');
-  booksGrid.innerHTML = '';
-    
-  for (let i = 0; i < myLibrary.length; i++){
+  const booksGrid = document.querySelector(".books-grid");
+  booksGrid.innerHTML = "";
+
+  for (let i = 0; i < myLibrary.length; i++) {
     const book = myLibrary[i];
-    const bookCard = document.createElement('div')
-    const title = document.createElement('p')
-    const author = document.createElement('p')
-    const pages = document.createElement('p')
-    const buttonGroup = document.createElement('div')
-    const readBtn = document.createElement('button')
-    const removeBtn = document.createElement('button')
+    const bookCard = document.createElement("div");
+    const title = document.createElement("p");
+    const author = document.createElement("p");
+    const pages = document.createElement("p");
+    const buttonGroup = document.createElement("div");
+    const readBtn = document.createElement("button");
+    const removeBtn = document.createElement("button");
 
-    bookCard.classList.add('book-card')
-    buttonGroup.classList.add('button-group')
-    readBtn.classList.add('btn', 'readToggle')
-    removeBtn.classList.add('btn')
+    bookCard.classList.add("book-card");
+    buttonGroup.classList.add("button-group");
+    readBtn.classList.add("btn", "readToggle");
+    removeBtn.classList.add("btn");
 
-    readBtn.setAttribute('onclick', `toggleRead(${i})`)
-    removeBtn.setAttribute('onclick', `removeBook(${i})`)
+    readBtn.setAttribute("onclick", `toggleRead(${i})`);
+    removeBtn.setAttribute("onclick", `removeBook(${i})`);
 
-    title.textContent = `"${book.title}"`
-    author.textContent = book.author
-    pages.textContent = `${book.pages} pages`
-    removeBtn.textContent = 'Remove'
+    title.textContent = `"${book.title}"`;
+    author.textContent = book.author;
+    pages.textContent = `${book.pages} pages`;
+    removeBtn.textContent = "Remove";
 
     if (book.read) {
-      readBtn.textContent = 'Read'
-      readBtn.classList.add('btn-light-green')
+      readBtn.textContent = "Read";
+      readBtn.classList.add("btn-light-green");
     } else {
-      readBtn.textContent = 'Not read'
-      readBtn.classList.add('btn-light-red')
+      readBtn.textContent = "Not read";
+      readBtn.classList.add("btn-light-red");
     }
 
-    bookCard.appendChild(title)
-    bookCard.appendChild(author)
-    bookCard.appendChild(pages)
-    buttonGroup.appendChild(readBtn)
-    buttonGroup.appendChild(removeBtn)
-    bookCard.appendChild(buttonGroup)
-    booksGrid.appendChild(bookCard)
+    bookCard.appendChild(title);
+    bookCard.appendChild(author);
+    bookCard.appendChild(pages);
+    buttonGroup.appendChild(readBtn);
+    buttonGroup.appendChild(removeBtn);
+    bookCard.appendChild(buttonGroup);
+    booksGrid.appendChild(bookCard);
   }
-};
+}
 
 function closeAddBookModal() {
-  addBookModal.classList.remove('active');
-  overlay.classList.remove('active');
-};
-
+  addBookModal.classList.remove("active");
+  overlay.classList.remove("active");
+}
 
 const handleKeyboardInput = (e) => {
-  if (e.key === 'Escape') closeAddBookModal()
-}
+  if (e.key === "Escape") closeAddBookModal();
+};
 
 window.onkeydown = handleKeyboardInput;
 overlay.onclick = closeAddBookModal;
